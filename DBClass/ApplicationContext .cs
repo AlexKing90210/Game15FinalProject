598fabd55e8e9f4ed011bac39a7615300dc6e062
AlexKing90210 <Alex.king90@yandex.ru>
2022-11-16 01:33:48 +0400

    Added all tables in database!

--- DBClass/ApplicationContext .cs
+++ DBClass/ApplicationContext .cs
@@ -5,6 +5,8 @@ namespace DBClass
     public class ApplicationContext : DbContext
     {
         public DbSet<Winners> Winners { get; set; } = null!;
+        public DbSet<DBCurrentGame> DBCurrentGame { get; set; } = null!;
+        public DbSet<DBGameSettings> DBGameSettings { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=Game15.db");
